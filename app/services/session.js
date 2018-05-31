76823d27766464c6fa787f32e324cc1edbd3f5b2
Chad Carbert <chadcarbert@me.com>
2018-05-31 20:52:32 +0200

    Add call-with action helper and example

--- app/services/session.js
+++ app/services/session.js
@@ -3,8 +3,9 @@ import Service from '@ember/service';
 export default Service.extend({
   isAuthenticated: true,
 
-  logout() {
+  logout(message) {
+    message = message || 'you will be logged out now';
+    alert(message);
     this.set('isAuthenticated', false);
-    this.set('isAdmin', false);
   }
 });
