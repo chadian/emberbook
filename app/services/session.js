781789977d3e34f54dedd1630c968125da272efa
Chad Carbert <chadcarbert@me.com>
2018-06-01 21:26:07 +0200

    Touch up header with better look & feel with ember-paper

--- app/services/session.js
+++ app/services/session.js
@@ -1,11 +1,13 @@
 import Service from '@ember/service';
+import { inject as service } from "@ember/service";
 
 export default Service.extend({
   isAuthenticated: true,
+  paperToaster: service(),
 
   logout(message) {
     message = message || 'you will be logged out now';
-    alert(message);
+    this.get('paperToaster').show(message);
     this.set('isAuthenticated', false);
   },
 
