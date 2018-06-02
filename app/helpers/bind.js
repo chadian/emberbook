2644a16e97a31b7138fc748e1d13eee8238af8f8
Chad Carbert <chadcarbert@me.com>
2018-06-02 19:33:02 +0200

    Rename call-with helper to bind, and add call helper

---
+++ app/helpers/bind.js
@@ -0,0 +1,8 @@
+import { helper } from '@ember/component/helper';
+import { get } from '@ember/object';
+
+export function callWith([ref, lookup, ...args]) {
+  return () => get(ref, lookup).call(ref, ...args);
+}
+
+export default helper(callWith);
