fb6f2b571959d453f8630f49f0c9460c25d7e0e3
Chad Carbert <chadcarbert@me.com>
2018-06-03 22:17:04 +0200

    WIP defrerred transition

--- app/components/friend-card.js
+++ app/components/friend-card.js
@@ -1,4 +1,14 @@
 import Component from '@ember/component';
+import { readOnly } from '@ember/object/computed';
 
 export default Component.extend({
+  loadingTask: null,
+  isLoading: readOnly('loadingTask.isRunning'),
+
+  actions: {
+    onClick() {
+      const task = this.get('onDetailsClick')();
+      this.set('loadingTask', task);
+    }
+  }
 });
--- app/controllers/friends-plus.js
+++ app/controllers/friends-plus.js
@@ -1,5 +1,20 @@
 import FriendsController from './friends';
+import { task, timeout } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
 
 export default FriendsController.extend({
-  _warpThreshold: 0
+  _warpThreshold: 0,
+  router: service(),
+
+  cacheThenTransition: task(function * (id) {
+    yield timeout(5000);
+    console.log('finally loaded', id);
+    this.get('router').transitionTo('index');
+  }).restartable(),
+
+  actions: {
+    transitionToFriend(id) {
+      return this.get("cacheThenTransition").perform(id);
+    }
+  }
 });
