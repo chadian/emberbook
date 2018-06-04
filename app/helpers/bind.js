2923c8c09f43bd057aab234f4b11dce1014c1f88
Chad Carbert <chadcarbert@me.com>
2018-06-04 07:14:47 +0200

    Fix bind helper to capture initial args and call arguments, add arity option

--- app/helpers/bind.js
+++ app/helpers/bind.js
@@ -1,8 +1,16 @@
 import { helper } from '@ember/component/helper';
 import { get } from '@ember/object';
 
-export function callWith([ref, lookup, ...args]) {
-  return () => get(ref, lookup).call(ref, ...args);
+export function bind([ref, lookup, ...args], { arity }) {
+  return function() {
+    let combinedArgs = [...args, ...arguments];
+
+    if (Number.isInteger(arity)) {
+      combinedArgs = combinedArgs.slice(0, arity);
+    }
+
+    return get(ref, lookup).call(ref, ...combinedArgs);
+  };
 }
 
-export default helper(callWith);
+export default helper(bind);
